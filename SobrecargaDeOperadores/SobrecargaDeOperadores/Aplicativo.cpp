568bd1d77208ac528ca7981840c0921dfcd4e758
Madely Betancourt <madelybetancourt@gmail.com>
2020-06-08 17:53:09 -0500

    Sobrecarga de operador mayor que

---
+++ SobrecargaDeOperadores/SobrecargaDeOperadores/Aplicativo.cpp
@@ -0,0 +1,201 @@
+/*#include <iostream>
+
+using namespace std;
+
+class Pareja {
+public:
+	int a, b;
+public:
+	Pareja operator+ (const Pareja& a, const Pareja& b);
+	Pareja () : a(0), b(0){
+	}
+
+	Pareja(const int a, const int b);
+
+	Pareja(const Pareja&);
+};
+
+Pareja::Pareja(const int a, const int b) {
+	this->a = a;
+	this->b = b;
+}
+
+Pareja::Pareja(const Pareja& p)
+{
+	*this = p;
+}
+
+Pareja& operator+ (const Pareja &A, const Pareja &B) {
+	Pareja C;
+	C.a = A.a + B.a;
+	C.b = A.b + A.b;
+}*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdio.h>
+using namespace std;
+class Pareja {
+
+public:
+    int a, b;
+
+public:
+    // constructor base
+    Pareja() : a(0), b(0) {}
+
+    // constructor parametrizado
+    Pareja(const int a, const int b) {
+        this->a = a;
+        this->b = b;
+    }
+
+    // constructor de copia
+    Pareja(const Pareja&);
+
+    // operadores miembros
+    Pareja& operator + (const Pareja& p);
+    Pareja& operator - (const Pareja& p);
+    Pareja& operator * (const Pareja& p);
+    Pareja& operator / (const Pareja& p);
+    Pareja& operator = (const Pareja& p);
+    Pareja& operator ++();
+    bool    operator ==(const Pareja& p) const;
+    bool operator > (const Pareja& p) const ;
+
+    // operadores no miembros
+    friend ostream& operator << (ostream& o, const Pareja& p);
+    friend istream& operator >> (istream& o, const Pareja& p);
+};
+
+// implementacion de los operadores para la clase Pareja
+//....................................
+Pareja::Pareja(const Pareja& p)
+{
+    *this = p;
+}
+//....................................
+Pareja& Pareja::operator + (const Pareja& p)
+{
+    this->a += p.a;
+    this->b += p.b;
+    return *this;
+}
+//....................................
+Pareja& Pareja::operator - (const Pareja& p)
+{
+    this->a -= p.a;
+    this->b -= p.b;
+    return *this;
+}
+//....................................
+Pareja& Pareja::operator * (const Pareja& p)
+{
+    this->a *= p.a;
+    this->b *= p.b;
+    return *this;
+}
+//....................................
+Pareja& Pareja::operator / (const Pareja& p)
+{
+    if (p.a != 0) this->a /= p.a;
+    if (p.b != 0) this->b /= p.b;
+    return *this;
+}
+//....................................
+Pareja& Pareja::operator = (const Pareja& p)
+{
+    if (this != &p) { //Comprueba que no se esté intentanod igualar un objeto a sí mismo
+        if (p.a != 0) this->a = p.a;
+        if (p.b != 0) this->b = p.b;
+    }
+    return *this;
+}
+
+//....................................
+Pareja& Pareja::operator ++ ()
+{
+    this->a++;
+    this->b++;
+    return *this;
+}
+
+//....................................
+bool Pareja::operator == (const Pareja& p) const
+{
+    return this->a == p.a && this->b == p.b;
+}
+
+bool Pareja::operator > (const Pareja& p) const {    // sobrecarga operador >
+    return this->a > p.a && this->b > p.b;
+}
+
+// implemetaci¢n de operadores no miembros
+ostream& operator << (ostream& o, const Pareja& p)
+{
+    o << "(" << p.a << ", " << p.b << ")";
+    return o;
+}
+
+istream& operator >> (istream& i, Pareja& p)
+{
+    cout << "Introducir valores para ( a, b) :";
+    i >> p.a >> p.b;
+    i.ignore();
+    return i;
+}
+int main(int argc, char** argv) {
+    Pareja A(150, 75);
+    Pareja B(100, 15);
+    Pareja C;
+    cout << "A = " << A << "\n";
+    cout << "B = " << B << "\n";
+    cout << "........................." << endl;
+    C = C + A;
+    C = C + B;
+    cout << "Suma C = " << C << endl;
+    cout << "........................." << endl;
+    C = A ;
+    C = C - B;
+    //cout << "A = " << A << "\n";
+    cout << "Resta C = " << C << endl;
+    cout << "........................." << endl;
+    C = A;
+    C = C * B;
+    //cout << "A = " << A << "\n";
+    cout << "Multi C = " << C << endl;
+    cout << "........................." << endl;
+    C = A;
+    C = C / B;
+    //cout << "A = " << A << "\n";
+    cout << "Divi C = " << C << endl;
+    cout << "........................." << endl;
+    /*C = A / B;
+    //cout << "A = " << A << "\n";
+    cout << "C = " << C << endl;
+    cout << "........................." << endl;*/
+    ++C;
+    cout << "Mas Mas C = " << C << endl;
+    cout << "A == B " << ((A == B) ? "  True \n" : "  False  \n");
+    cout << "........................." << endl;
+    C = A;
+    cout << "A > B " << ((A > B) ? "  True \n" : "  False  \n");
+    cout << "........................." << endl;
+    C = A = B = ++C;
+    cout << "A = " << A << "\n";
+    cout << "B = " << B << "\n";
+    cout << "B = " << B << endl;
+    cout << "........................." << endl;
+    return 0;
+}
+
+
+
+
+/*int main() {
+	Pareja* A = new Pareja(100, 100);
+	Pareja* B = new Pareja(50, 50);
+	Pareja* C;
+	C = A + B;
+}*/
